df03c7f34765c3fa1422fe9b5a9178e6192324d5
Maryam Pourreza <mpourrez@andrew.cmu.edu>
2025-09-03 16:22:39 -0700

    Fix issues with protos

--- protos/benchmark_pb2_grpc.py
+++ protos/benchmark_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import benchmark_pb2 as benchmark__pb2
+from protos import benchmark_pb2 as benchmark__pb2
 
 GRPC_GENERATED_VERSION = '1.74.0'
 GRPC_VERSION = grpc.__version__
